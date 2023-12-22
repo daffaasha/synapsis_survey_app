99cfdbe23d0474514f09a2ab004c158e8df4b8ec
daffaasha <aadaffa41@gmail.com>
2023-12-22 21:17:54 +0700

    update: main for various impl
    - impl the service locator
    - both auth bloc and login bloc
    - and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:synapsis_survey_app/config/route/routes.dart';
+import 'package:synapsis_survey_app/injection_container.dart';
+import 'package:synapsis_survey_app/presentation/bloc/login_page_bloc/authentication_bloc/authentication_bloc.dart';
+import 'package:synapsis_survey_app/presentation/bloc/login_page_bloc/login_bloc/login_bloc.dart';
+import 'package:synapsis_survey_app/presentation/pages/login/login_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDependencies();
   runApp(const MainApp());
 }
 
@@ -9,11 +17,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LoginBloc>(
+          create: (context) => LoginBloc(),
         ),
+        BlocProvider<AuthenticationBloc>(
+          create: (create) => sl<AuthenticationBloc>(),
+        ),
+      ],
+      child: const MaterialApp(
+        onGenerateRoute: AppRoute.onGeneralRoutes,
+        home: LoginPage(),
       ),
     );
   }
