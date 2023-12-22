a85fa7a85150cf66df32e9ae3d26ab3ed1ab1365
daffaasha <aadaffa41@gmail.com>
2023-12-22 21:15:59 +0700

    add: resource const values
    color values for holding all the color resources
    dimens value for holding the often used paddings

---
+++ lib/config/theme/color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color primaryBlue = Color(0xFF1FA0C9);
+
+const Color softBlue = Color(0xFFD6E4EC);
+const Color softBlue2 = Color(0xFFD0D7DE);
+
+const Color gray = Color(0xFF757575);
+
+const Color softGray = Color(0xFFB9B9B9);
+const Color softGray2 = Color(0xFFFBFBFB);
+
+const Color red = Color(0xFFE74C3C);
---
+++ lib/config/theme/dimens.dart
@@ -0,0 +1,7 @@
+const smallPadding1 = 8.0;
+const smallPadding2 = 12.0;
+
+const mediumPadding1 = 24.0;
+const mediumPadding2 = 32.0;
+
+const largePadding1 = 48.0;
