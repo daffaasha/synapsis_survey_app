e6e3927a9a559c76da8864d75873c92b44d69bcb
daffaasha <aadaffa41@gmail.com>
2023-12-23 18:09:22 +0700

    update: update the injection for the dio migration

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:get_it/get_it.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:synapsis_survey_app/data/data_sources/remote/dio_config.dart';
 import 'package:synapsis_survey_app/data/data_sources/remote/survey_api.dart';
 import 'package:synapsis_survey_app/data/repository/repository.dart';
 import 'package:synapsis_survey_app/domain/repository/repository.dart';
@@ -10,7 +17,14 @@ import 'package:synapsis_survey_app/presentation/bloc/login_page_bloc/authentica
 final sl = GetIt.instance;
 
 Future<void> initializeDependencies() async {
-  sl.registerSingleton(const ApiService());
+  final dio = Dio();
+  final dioConfig = DioConfig(dio);
+  await dioConfig.configDio();
+
+  //Dio
+  sl.registerLazySingleton(() => dioConfig.dio);
+
+  sl.registerSingleton(ApiService(sl()));
 
   sl.registerSingleton<Repository>(RepositoryImpl(sl()));
 
